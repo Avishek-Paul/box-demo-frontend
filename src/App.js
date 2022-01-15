061f13ccf0128dc9cfc872fc38fdb52322642994
Avishek Paul <avishek97paul@gmail.com>
2022-01-14 22:36:11 -0500

    create uploader component file

--- src/App.js
+++ src/App.js
@@ -7,34 +7,26 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
+  extendTheme
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Uploader from './components/Uploader'
+
+const colors = {
+  brand: {
+    900: '#0065b3',
+    800: '#e61030',
+    700: '#FFFFFF',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Uploader />
     </ChakraProvider>
   );
 }
